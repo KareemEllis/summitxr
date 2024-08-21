63a9efbf7ba8a242db2cb2b5252acd2ca8d63dd8
Kareem Ellis <kareemellis55@gmail.com>
2024-08-21 13:11:01 -0500

    Added CameraButton component

--- src/CameraButton.tsx
+++ src/CameraButton.tsx
@@ -34,3 +34,80 @@ document.addEventListener('DOMContentLoaded', () => {
     sceneEl.addEventListener('loaded', sceneLoaded);
   }
 });
+
+interface Props {
+  entity?: string;
+}
+
+export const CameraButton: Component<Props> = (props) => {
+  const iconOff = createMemo(() => {
+    return !cameraEnabled();
+  });
+
+  const title = createMemo(() => {
+    if (!iconOff()) {
+      return 'Disable Camera';
+    } else {
+      return 'Enable Camera';
+    }
+  });
+
+  onMount(() => {
+    if (NAF.connection.isConnected()) {
+      setIsConnected(true);
+    } else {
+      const listener = () => {
+        setIsConnected(true);
+        NAF.connection.adapter?.enableCamera?.(untrack(cameraEnabled));
+      };
+      document.body.addEventListener('connected', listener);
+      onCleanup(() => {
+        document.body.removeEventListener('connected', listener);
+      });
+    }
+  });
+
+  createEffect(() => {
+    if (!domContentLoaded()) return;
+    const info = { videoOff: iconOff() };
+    // @ts-ignore
+    document.querySelector(props.entity ?? '#player')?.setAttribute('player-info', info);
+  });
+
+  createEffect(() => {
+    const enabled = cameraEnabled();
+    localStorage.setItem('cameraEnabled', enabled.toString());
+    if (isConnected()) {
+      if (!NAF.connection.adapter?.enableCamera) {
+        console.error(
+          `The specified NAF adapter doesn't have the enableCamera method, please be sure you have networked-scene="adapter:easyrtc;video:true" options and networked-video-source on your avatar template.`,
+        );
+        return;
+      }
+      NAF.connection.adapter.enableCamera(enabled);
+    }
+  });
+
+  return (
+    <Show when={videoEnabled()}>
+      <button
+        class="btn-secondary btn-rounded"
+        classList={{ active: !iconOff() }}
+        onClick={() => {
+          setCameraEnabled((enabled) => !enabled);
+          // @ts-ignore
+          document.activeElement.blur();
+          document.body.focus();
+        }}
+        title={title()}
+      >
+        <Show when={!iconOff()}>
+          <BsCameraVideo size={24} />
+        </Show>
+        <Show when={iconOff()}>
+          <BsCameraVideoOff size={24} />
+        </Show>
+      </button>
+    </Show>
+  );
+};
