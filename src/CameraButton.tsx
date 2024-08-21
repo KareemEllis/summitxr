3224233279662da4d3d76bf66143fc05ed8b4fe1
Kareem Ellis <kareemellis55@gmail.com>
2024-08-21 12:50:20 -0500

    Modified eslint indent rule to warn (2 spaced indend)

--- src/CameraButton.tsx
+++ src/CameraButton.tsx
@@ -11,26 +11,26 @@ export const [videoEnabled, setVideoEnabled] = createSignal(false);
 const [domContentLoaded, setDomContentLoaded] = createSignal(false);
 
 document.addEventListener('DOMContentLoaded', () => {
-    setDomContentLoaded(true);
-    const sceneEl = document.querySelector('a-scene');
+  setDomContentLoaded(true);
+  const sceneEl = document.querySelector('a-scene');
 
-    const sceneLoaded = () => {
-      // @ts-ignore
-      const settings = sceneEl?.getAttribute('networked-scene');
-      // @ts-ignore
-      const adapter = settings.adapter;
-      if (adapter !== 'easyrtc' && adapter !== 'janus') return;
-      // @ts-ignore
-      if (adapter === 'easyrtc' && !settings.video) return;
-
-      setVideoEnabled(true);
-    };
-
+  const sceneLoaded = () => {
     // @ts-ignore
-    if (sceneEl.hasLoaded) {
-      sceneLoaded();
-    } else {
-      // @ts-ignore
-      sceneEl.addEventListener('loaded', sceneLoaded);
-    }
-  });
+    const settings = sceneEl?.getAttribute('networked-scene');
+    // @ts-ignore
+    const adapter = settings.adapter;
+    if (adapter !== 'easyrtc' && adapter !== 'janus') return;
+    // @ts-ignore
+    if (adapter === 'easyrtc' && !settings.video) return;
+
+    setVideoEnabled(true);
+  };
+
+  // @ts-ignore
+  if (sceneEl.hasLoaded) {
+    sceneLoaded();
+  } else {
+    // @ts-ignore
+    sceneEl.addEventListener('loaded', sceneLoaded);
+  }
+});
