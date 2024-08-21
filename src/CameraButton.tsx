90ef31ab46a8fdf67f8d8e4734dd7241bbc9a82f
Kareem Ellis <kareemellis55@gmail.com>
2024-08-21 12:48:24 -0500

    Added DOMContentLoaded event listener

--- src/CameraButton.tsx
+++ src/CameraButton.tsx
@@ -9,3 +9,28 @@ const [isConnected, setIsConnected] = createSignal(false);
 export const [videoEnabled, setVideoEnabled] = createSignal(false);
 
 const [domContentLoaded, setDomContentLoaded] = createSignal(false);
+
+document.addEventListener('DOMContentLoaded', () => {
+    setDomContentLoaded(true);
+    const sceneEl = document.querySelector('a-scene');
+
+    const sceneLoaded = () => {
+      // @ts-ignore
+      const settings = sceneEl?.getAttribute('networked-scene');
+      // @ts-ignore
+      const adapter = settings.adapter;
+      if (adapter !== 'easyrtc' && adapter !== 'janus') return;
+      // @ts-ignore
+      if (adapter === 'easyrtc' && !settings.video) return;
+
+      setVideoEnabled(true);
+    };
+
+    // @ts-ignore
+    if (sceneEl.hasLoaded) {
+      sceneLoaded();
+    } else {
+      // @ts-ignore
+      sceneEl.addEventListener('loaded', sceneLoaded);
+    }
+  });
