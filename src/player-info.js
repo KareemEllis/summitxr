6c9b2c70c457986133a87686c8bdabfe4232a3c8
Kareem Ellis <kareemellis55@gmail.com>
2024-09-01 02:06:17 -0500

    Added screenshare-template to player-info.js

--- src/player-info.js
+++ src/player-info.js
@@ -28,6 +28,23 @@ NAF.schemas.getComponents = (template) => {
     });
   }
 
+  if (!NAF.schemas.hasTemplate('#screenshare-template')) {
+    NAF.schemas.add({
+      template: '#screenshare-template',
+      components: [
+        {
+          component: 'position',
+          requiresNetworkUpdate: NAF.utils.vectorRequiresUpdate(0.001),
+        },
+        {
+          component: 'rotation',
+          requiresNetworkUpdate: NAF.utils.vectorRequiresUpdate(0.5),
+        },
+        'player-info',
+      ],
+    });
+  }
+
   const components = NAF.schemas.getComponentsOriginal(template);
   return components;
 };
